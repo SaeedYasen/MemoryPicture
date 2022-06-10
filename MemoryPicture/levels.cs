c4bbabab23f113166e19ac976e57cc4db1022378
SaeedYasen <107278609+SaeedYasen@users.noreply.github.com>
2022-06-10 23:36:14 +0300

    MemoryPicture

---
+++ MemoryPicture/levels.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemoryPicture
+{
+    public partial class levels : Form
+    {
+        Image LevelLock = Image.FromFile(@"..\..\pictures\\lock.png");
+        int level;
+        public levels()
+        {
+            InitializeComponent();
+        }
+        public levels(int lvl)
+        {
+            level = lvl;
+            IflEVEL();
+            InitializeComponent();
+        }
+
+        private bool whatLevel()
+        {
+            switch (level)
+            {
+                case 1:
+                    level2.BackgroundImage = LevelLock;
+                    level3.BackgroundImage = LevelLock;
+                    level4.BackgroundImage = LevelLock;
+                    level5.BackgroundImage = LevelLock;
+                    return true;
+                case 2:
+                    level3.BackgroundImage = LevelLock;
+                    level4.BackgroundImage = LevelLock;
+                    level5.BackgroundImage = LevelLock;
+                    return true;
+                case 3:
+                    level4.BackgroundImage = LevelLock;
+                    level5.BackgroundImage = LevelLock;
+                    return true;
+                case 4:
+                    level5.BackgroundImage = LevelLock;
+                    return true;
+            }
+            return false;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            lists ls = new lists();
+            ls.Show();
+        }
+
+        private void levels_Load(object sender, EventArgs e)
+        {
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+
+
+
+        }
+        private void level1_Click(object sender, EventArgs e)
+        {
+                GamerUpdate();
+        }
+
+        private void GamerUpdate()
+        {
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+            button4.Visible = true;
+
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+
+        }
+
+        public void IflEVEL()
+        {
+
+        }
+
+        private void level2_Click(object sender, EventArgs e)
+        {
+
+                GamerUpdate();
+
+        }
+
+        private void level3_Click(object sender, EventArgs e)
+        {
+
+                GamerUpdate();
+
+        }
+
+        private void level4_Click(object sender, EventArgs e)
+        {
+
+                GamerUpdate();
+        }
+
+        private void level5_Click(object sender, EventArgs e)
+        {
+                GamerUpdate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 f = new Form1();
+            this.Hide();
+            f.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            this.Hide();
+            f.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form3 f = new Form3();
+            this.Hide();
+            f.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form4 f = new Form4();
+            this.Hide();
+            f.Show();
+        }
+    }
+}
---
+++ MemoryPicture/login.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.OleDb;
+using System.Data;
+
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace MemoryPicture
+{
+    public partial class login : Form
+    {
+        string level = "1";
+        public string path = @"..\..\database\memorypic.accdb";
+
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void login_Load(object sender, EventArgs e)
+        {
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            lists f1 = new lists("user1",level);
+            f1.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            passlog.Visible = true;
+            usernamelog.Visible = true;
+            textBoxpasslog.Visible = true;
+            textBoxuserlog.Visible = true;
+            btnlogin.Visible = true;
+            button1.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            usernameup.Visible = true;
+            nameup.Visible = true;
+            passup.Visible = true;
+            boxusername.Visible = true;
+            textBoxname.Visible = true;
+            textBoxpassup.Visible = true;
+            sigupbtn.Visible = true;
+            button2.Visible = false;
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            OleDbConnection con = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = " + path);
+            OleDbCommand cmdoledb = con.CreateCommand();
+            OleDbDataAdapter da;
+            DataTable dt = new DataTable();
+            con.Open();
+
+
+            da = new OleDbDataAdapter("select * from player where username='" + textBoxuserlog.Text + "' and pas='" + textBoxpasslog.Text + "'", con);
+
+            try
+            {
+                da.Fill(dt);
+                if (dt.Rows.Count <= 0)
+                {
+
+                }
+                else if (dt.Rows.Count > 0)
+                {
+                    MessageBox.Show("Login Succsufully");
+                    this.Hide();
+                    lists f1 = new lists(textBoxuserlog.Text,level);
+                    f1.Show();
+                }
+                else MessageBox.Show("password or username false");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("" + ex);
+            }
+            dt.Clear();
+
+        }
+
+        private void sigupbtn_Click(object sender, EventArgs e)
+        {
+            OleDbConnection con = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = " + path);
+            OleDbCommand cmdoledb = con.CreateCommand();
+            con.Open();
+
+            String name = textBoxname.Text.ToString();
+            String username = boxusername.Text.ToString();
+            String pass = textBoxpassup.Text.ToString();
+            String lev = "1";
+
+            String my_querry = ("INSERT INTO player(name,username,pas) VALUES ('" + name + "','" + username + "','" + pass + "')");
+
+            try
+            {
+                OleDbCommand cmd = new OleDbCommand(my_querry, con);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("success .. and u sholed log in");
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            con.Close();
+        }
+    }
+}
